0659ab881ee397fa2a5586267968b76e1e5fcb8a
na57 <na57@qq.com>
2016-05-28 09:24:44 +0800

    完成v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,105 @@
 # wxe-auth-express
 express middleware, 用它可以实现微信企业号的登录
+
+## API
+### signin
+此中间件用的功能包括：
+1. 发起登录认证请求，详见[微信企业号文档](http://qydev.weixin.qq.com/wiki/index.php?title=OAuth%E9%AA%8C%E8%AF%81%E6%8E%A5%E5%8F%A3)
+2. 接收返回的code，并用code获取UserId
+
+#### 参数
+##### wxapi
+必须的。
+由 `wxent-api-redis`生成的微信企业号接口API，将被调用的接口包括：
+  - `getAuthorizeURL` 用于构造认证请求URL
+  - `getUserIdByCode` 用于用code获取UserId
+
+##### cookieNameForUserId
+登录后需要将（加密的）UserId存储到cookie中，此处用于指定cookie的名字，默认为`userId`。
+
+##### callbackUrl
+认证服务器返回code时使用的回调url，默认可以由系统自己构造一个当前url
+
+### getme
+用户读取当前用户的登录信息
+
+#### 参数
+##### cookieNameForUserId
+登录后需要将（加密的）UserId存储到cookie中，此处用于指定cookie的名字，默认为`userId`。
+
+### getUserId
+获取当前用户的id。
+成功获取时，将结果存放在`req.user.userid`，失败时，返回响应：`{ ret:-1 }`
+
+### getUser
+在使用此API时，必须先调用getUserId，或设置好`req.user.userid`的值。
+获取当前用户在微信企业号的详细信息。
+成功获取时，将结果存放在`req.user`中，数据结构见[微信企业号文档](http://qydev.weixin.qq.com/wiki/index.php?title=%E7%AE%A1%E7%90%86%E6%88%90%E5%91%98#.E8.8E.B7.E5.8F.96.E6.88.90.E5.91.98)；
+失败时，返回响应：{ ret: -1 }
+#### 参数
+##### wxapi
+必须的。
+由 `wxent-api-redis`生成的微信企业号接口API，将被调用的接口包括：
+  - `getUser` 用于获取用户数据
+
+## Build
+使用`npm run build`
+
+## 示例
+### Express处理程序
+
+```javascript
+import { Router } from 'express';
+import API from 'wxent-api-redis';
+import { signin, getme } from 'wxe-auth-express';
+
+const wxapi = API(corpId, secret, agentId, redishost, redisport);
+const router = new Router();
+
+const host = 'wx.example.com';
+
+router.get('/signin', signin({
+  wxapi: wxapi,
+  cookieNameForUserId: 'userId',
+  callbackUrl: `http://${host}/signin`
+}));
+
+// 获取当前登录用户信息
+router.get('/me', getme('userId'));
+```
+
+### 客户端
+
+```javascript
+let getMe = async() => {
+  // fetch的时候必须带上cookie
+  let meRes = await fetch(`/me`, {
+    credentials: 'same-origin'
+  });
+  let result = await meRes.json();
+
+  // 获取到用户信息，用户已经登录
+  if(result.ret === 0){
+    return Promise.resolve(result.data);
+  }
+    // 不能获取到用户信息，用户未登录
+    else {
+    return Promise.reject(await result.msg);
+  }
+}
+
+// 检查用户是否已经登录
+getMe().then(userId => {
+  // 用户已经登录
+  // ...
+}).catch(e => {
+  // 用户还未登录，转到登录页面
+  // 此时必须带上redirect_uri参数，这样才知道认证之后要返回哪个页面
+  window.location = `/api/signup/signin?redirect_uri=${window.location.href}`;
+})
+```
+
+## 在本地进行调试
+在本地进行调试需要注意的问题：
+1. 在本地进行调试需要使用[微信Web开发者工具]()；
+2. 需要修改本地hosts文件，将可信域名绑定到
---
+++ src/index.js
@@ -0,0 +1,102 @@
+
+/*
+实现从微信企业号登录
+ */
+export const signin = ({ wxapi, cookieNameForUserId = 'userId', callbackUrl }) => {
+  return (req, res) => {
+    if (!callbackUrl) {
+      callbackUrl = `${req.protocol}://${req.get('Host')}${req.originalUrl}}`
+    }
+    // 1. 判断是否带有code参数，如果是的话，则说明已经从认证服务器返回
+    if ('code' in req.query){
+      // 使用state验证请求是否合法
+      if (req.signedCookies.state === req.query.state)
+        // 使用code获取userId
+        wxapi.getUserIdByCode(req.query.code, (err, result) => {
+          if(err) {
+            res.send({ret: -1, msg: err});
+          } else {
+            // 删除state 和 redirect_uri 的 cookie 值
+            res.clearCookie('state');
+            res.clearCookie('redirect_uri');
+
+            // 用户验证正确，设置用户状态为登录，返回原URL
+            res.cookie(cookieNameForUserId, result.UserId, {maxAge: 24*3600*1000*365, signed: true});
+            res.redirect(req.signedCookies.redirect_uri);
+          }
+        });
+      else {
+        res.send({ret: -1, msg: 'bad state value and illegel request.'});
+      }
+    }
+    // 2. 无'code'参数，转向认证服务器进行认证
+    else {
+      // 2.1 缓存 state 和redirect_uri
+      let {redirect_uri} = req.query;
+      if(redirect_uri) {
+        let state = Math.random().toString();
+
+        // 将state 和 redirect_uri 存入cookie中
+        res.cookie('state', state, {maxAge: 30000, signed: true});
+        res.cookie('redirect_uri', redirect_uri, {maxAge:30000, signed: true});
+
+        // 2.2 获取认证url
+        let url = wxapi.getAuthorizeURL(callbackUrl, state, 'snsapi_base');
+
+        // 2.3 返回客户端，并在转到认证服务器。
+        // 此处不能直接跳转到认证url，因为这样cookie就无法在客户端生效。
+        res.send(`
+          <html>
+            <head>
+              <meta http-equiv="refresh" content="0; url=${url}" />
+            </head>
+          </html>
+        `);
+      }
+      else {
+        res.send({ret: -1, msg: 'redirect_uri must be provided.'});
+      }
+    }
+  }
+}
+
+/*
+获取当前登录用户的userId
+ */
+export const getme = ({ cookieNameForUserId = 'userId' } = {}) => {
+  return (req, res) => {
+    const userId = req.signedCookies[cookieNameForUserId];
+    if (userId) res.send({ ret: 0, data: userId });
+    else res.send({ ret: -1, msg: 'You haven\'t sign in.' });
+  };
+};
+
+/*
+获取当前登录用户的id，结果存放于req.user.userid，未能获取的时候直接返回响应
+ */
+export const getUserId = (cookieNameForUserId = 'userId') => (req, res, next) => {
+  req.user = Object.assign({}, req.user, {});
+  req.user.userid = req.signedCookies[cookieNameForUserId];
+  if (req.user.userid) next();
+  else res.send({ ret: -1, msg: 'not logged' });
+};
+
+/*
+获取当前用户在微信企业号的详细信息。结果存放在req.user中，数据结构详见：
+前提：必须首先调用getUserId
+ */
+export const getUser = ({ wxapi }) => (req, res, next) => {
+  if ('user' in req && req.user.userid) {
+    wxapi.getUser(req.user.userid, (err, result) => {
+      if (err) {
+        Object.assign(req.user, {
+          errcode: 500,
+          errmsg: err,
+        });
+      } else Object.assign(req.user, result);
+      next();
+    });
+  } else {
+    throw new Error('req.user.userid must be provided.');
+  }
+};
